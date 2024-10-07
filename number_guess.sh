f46d8355e1e1455bcdcf0b7162771809fc4eb023
jsilo <joeyjoesilo@gmail.com>
2024-10-07 19:09:20 -0400

    feat: implement while loop for guessing the secret number

--- number_guess.sh
+++ number_guess.sh
@@ -23,5 +23,29 @@ if [[ $does_username_exist == "t" ]]; then
 else
   echo -e "\nWelcome, $username! It looks like this is your first time here.\n"
 
-  $PSQL "INSERT INTO usernames(username) VALUES('$username');" 
+  $PSQL "INSERT INTO usernames(username) VALUES('$username');"
 fi
+
+echo -e "\nGuess the secret number between 1 and 1000:"
+
+read guess
+number_of_guesses=0
+
+while [[ true ]]; do
+
+  (( number_of_guesses++ ))
+
+  if (( guess < $random_num )); then
+    echo -e "\nIt's higher than that, guess again:"
+
+    read guess
+  elif (( guess > $random_num )); then
+    echo -e "\nIt's lower than that, guess again:"
+    
+    read guess
+  else
+    echo "You guessed it in $number_of_guesses tries. The secret number was $random_num. Nice job!"
+
+    break
+  fi
+done
