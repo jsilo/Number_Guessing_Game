4973d3607bd1d87b703c69a3041840738d310291
jsilo <joeyjoesilo@gmail.com>
2024-10-08 08:33:19 -0400

    refactor: apply DRY principle in while loop and make guess_validator overwrite the global variable 'guess'

--- number_guess.sh
+++ number_guess.sh
@@ -26,15 +26,11 @@ else
   $PSQL "INSERT INTO usernames(username) VALUES('$username');"
 fi
 
-echo -e "\nGuess the secret number between 1 and 1000:"
-
-read guess
-
 guess_validator() {
 
   local validated_guess=$1
 
-  while [[ true ]]; do
+  while true; do
     if [[ $validated_guess =~ ^[[:digit:]]+$ ]]; then
       echo -e "\nThe guess is a pure integer."
       break
@@ -44,30 +40,31 @@ guess_validator() {
     fi
   done
 
-  echo -e "\nExiting the guess_validator function. The validated guess is: $validated_guess\n"
+  echo -e "\nExiting the guess_validator function. The validated guess is: $validated_guess\nNow overwriting global variable 'guess'.\n"
+
+  guess=$validated_guess
 
 }
 
-guess_validator $guess
-
 number_of_guesses=0
 
-while [[ true ]]; do
+echo -e "\nGuess the secret number between 1 and 1000:"
 
+while true; do
+  
+  read guess
+  guess_validator $guess
+
+  echo -e "Start of while loop. \$guess is: $guess"
+  
   (( number_of_guesses++ ))
 
   if (( guess < $random_num )); then
     echo -e "\nIt's higher than that, guess again:"
 
-    read guess
-    guess_validator $guess
-
   elif (( guess > $random_num )); then
     echo -e "\nIt's lower than that, guess again:"
     
-    read guess
-    guess_validator $guess
-
   else
     echo "You guessed it in $number_of_guesses tries. The secret number was $random_num. Nice job!"
 
